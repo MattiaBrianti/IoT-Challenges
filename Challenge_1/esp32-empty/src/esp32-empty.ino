23efa322fe9bb579a6263103cece1a3a437a7dcd
MattiaBrianti <mattia.brianti@mail.polimi.it>
2026-03-18 16:54:50 +0100

    coded json and esp32

---
+++ Challenge_1/esp32-empty/src/esp32-empty.ino
@@ -0,0 +1,67 @@
+#include <WiFi.h>
+#include <esp_now.h>
+
+// Define PINs
+#define PIR_PIN 27
+#define LDR_PIN 34
+
+// MAC receiver
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+esp_now_peer_info_t peerInfo;
+
+// Parameters for DeepSleep
+#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  1.4        /* Personal code ending in 59 */
+
+// Sending callback
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("Send Status: ");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Ok" : "Error");
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Setting up the PIR and LDR pins
+  pinMode(PIR_PIN, INPUT);
+  pinMode(LDR_PIN, INPUT);
+
+  Wifi.mode(WIFI_STA);
+  esp_now_init();
+
+  // send callback
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Peer Registration
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  esp_now_add_peer(&peerInfo);
+
+  // Read the PIR and LDR values
+  int motionDetected = digitalRead(PIR_PIN);
+  int luminosity = analogRead(LDR_PIN);
+
+  // Create the message to send
+  String message;
+  if (motionDetected == HIGH) {
+    message = "MOTION_DETECTED-LUMINOSITY:" + String(luminosity);
+  } else {
+    message = "MOTION_NOT_DETECTED-LUMINOSITY:" + String(luminosity);
+  }
+
+  // Send the message via ESP-NOW
+  esp_now_send(broadcastAddress, (uint8_t*)message.c_str(), message.lenght()+1);
+
+  delay(100); // Short delay to ensure the message is sent before sleeping
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  esp_deep_sleep_start();
+}
+
+void loop() {
+  //leave empty, as we are using deep sleep and the device will reset after waking up
+}
